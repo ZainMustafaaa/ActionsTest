188ee0c61751b82cc71e979e504e03c340496f5b
ZainMustafaaa <zainmustafaaa@hotmail.com>
2020-10-13 11:33:21 +0500

    update

--- .github/secrets/decrypt_secrets__.sh
+++ .github/secrets/decrypt_secrets__.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+set -eo pipefail
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
 
 
 security create-keychain -p "" build.keychain
@@ -14,5 +15,3 @@ security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain

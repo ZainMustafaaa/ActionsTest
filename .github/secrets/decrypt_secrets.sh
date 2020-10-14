0117b4aae0cdc3e6e8306e879461b86b89adda09
ZainMustafaaa <zainmustafaaa@hotmail.com>
2020-10-14 16:38:41 +0500

    v97

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
-set -eo pipefail
+# set -eo pipefail
 
 # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/distributionv1.mobileprovision ./.github/secrets/distributionv1.mobileprovision.gpg
 # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/certificate.p12 ./.github/secrets/certificate.p12.gpg
+
+# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+# cp ./.github/secrets/distributionv1.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/distributionv1.mobileprovision
+
+
+# security create-keychain -p certificate.p12 build.keychain
+# security import ./.github/secrets/certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "12345" -A
+
+# security list-keychains -s ~/Library/Keychains/build.keychain
+# security default-keychain -s ~/Library/Keychains/build.keychain
+# security unlock-keychain -p "ios" ~/Library/Keychains/build.keychain
+ 
+# #!/bin/sh 
+
+# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/distributionv1.mobileprovision ./.github/secrets/distributionv1.mobileprovision.gpg
+# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/certificate.p12 ./.github/secrets/certificate.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
@@ -10,27 +27,10 @@ cp ./.github/secrets/distributionv1.mobileprovision ~/Library/MobileDevice/Provi
 
 
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "12345" -A
+security import ./.github/secrets/certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
- 
-# #!/bin/sh 
 
-# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/distributionv1.mobileprovision ./.github/secrets/distributionv1.mobileprovision.gpg
-# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/certificate.p12 ./.github/secrets/certificate.p12.gpg
-
-# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
-# cp ./.github/secrets/distributionv1.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/distributionv1.mobileprovision
-
-
-# security create-keychain -p "" build.keychain
-# security import ./.github/secrets/certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
-
-# security list-keychains -s ~/Library/Keychains/build.keychain
-# security default-keychain -s ~/Library/Keychains/build.keychain
-# security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-
-# security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain

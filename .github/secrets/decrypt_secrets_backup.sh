68ab009b653139b41f9556066bed8587d2b0e3ee
ZainMustafaaa <zainmustafaaa@hotmail.com>
2020-10-15 13:28:02 +0500

    v102

--- .github/secrets/decrypt_secrets_backup.sh
+++ .github/secrets/decrypt_secrets_backup.sh
@@ -25,21 +25,12 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision
 
-KEYCHAIN="MyApp$$.keychain"
-KEYCHAIN_PASSWORD="MyApp"
-security create-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
-security list-keychains -d user -s "$KEYCHAIN" $(security list-keychains -d user | sed s/\"//g)
-security list-keychains
 
-# Unlock keychain
-security set-keychain-settings "$KEYCHAIN"
-security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
-security import ./.github/secrets/Certificate.p12 -t agg -k "$KEYCHAIN" -P "" -T "/usr/bin/codesign"
-
-# Detect the iOS identity
-IOS_IDENTITY=$(security find-identity -v -p codesigning "$KEYCHAIN" | head -1 | grep '"' | sed -e 's/[^"]*"//' -e 's/".*//')
-IOS_UUID=$(security find-identity -v -p codesigning "$KEYCHAIN" | head -1 | grep '"' | awk '{print $2}')
-
-security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain

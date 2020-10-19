a01de802a630412d7f52f8b41b2e1ef0e8007318
ZainMustafaaa <zainmustafaaa@hotmail.com>
2020-10-19 14:59:14 +0500

    update

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,32 +1,10 @@
-#!/bin/sh
-# set -eo pipefail
-
-# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision.gpg
-# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificate.p12 ./.github/secrets/Certificate.p12.gpg
-
-# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
-# cp ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision
-
-
-# security create-keychain -p Certificate.p12 build.keychain
-# security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "12345" -A
-
-# security list-keychains -s ~/Library/Keychains/build.keychain
-# security default-keychain -s ~/Library/Keychains/build.keychain
-# security unlock-keychain -p "ios" ~/Library/Keychains/build.keychain
- 
-# #!/bin/sh 
-
-# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision.gpg
-# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificate.p12 ./.github/secrets/Certificate.p12.gpg
-
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision
-
+cp ./.github/secrets/provissioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/provissioning.mobileprovision
+# bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision
 KEYCHAIN="MyApp$$.keychain"
 KEYCHAIN_PASSWORD="MyApp"
+
 security create-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
 security list-keychains -d user -s "$KEYCHAIN" $(security list-keychains -d user | sed s/\"//g)
 security list-keychains
@@ -34,7 +12,6 @@ security list-keychains
 # Unlock keychain
 security set-keychain-settings "$KEYCHAIN"
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
-
 
 security import ./.github/secrets/Certificate.p12 -t agg -k "$KEYCHAIN" -P "" -T "/usr/bin/codesign"
 

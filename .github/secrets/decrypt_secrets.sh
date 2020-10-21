1b8c5b438b9ce12a09fa4ea1f55d67bb3000aa13
ZainMustafaaa <zainmustafaaa@hotmail.com>
2020-10-21 10:57:34 +0500

    update v104

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,9 +1,32 @@
+#!/bin/sh
+# set -eo pipefail
+
+# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificate.p12 ./.github/secrets/Certificate.p12.gpg
+
+# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+# cp ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision
+
+
+# security create-keychain -p Certificate.p12 build.keychain
+# security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "12345" -A
+
+# security list-keychains -s ~/Library/Keychains/build.keychain
+# security default-keychain -s ~/Library/Keychains/build.keychain
+# security unlock-keychain -p "ios" ~/Library/Keychains/build.keychain
+ 
+# #!/bin/sh 
+
+# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision ./.github/secrets/bc07e9c5-bf69-404b-ad84-73d9f80aea1b.mobileprovision.gpg
+# # gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificate.p12 ./.github/secrets/Certificate.p12.gpg
+
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/provissioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/provissioning.mobileprovision
+
 KEYCHAIN="MyApp$$.keychain"
 KEYCHAIN_PASSWORD="MyApp"
-
 security create-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
 security list-keychains -d user -s "$KEYCHAIN" $(security list-keychains -d user | sed s/\"//g)
 security list-keychains
@@ -12,10 +35,12 @@ security list-keychains
 security set-keychain-settings "$KEYCHAIN"
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
 
+
 security import ./.github/secrets/Certificate.p12 -t agg -k "$KEYCHAIN" -P "" -T "/usr/bin/codesign"
 
 # Detect the iOS identity
 IOS_IDENTITY=$(security find-identity -v -p codesigning "$KEYCHAIN" | head -1 | grep '"' | sed -e 's/[^"]*"//' -e 's/".*//')
-IOS_UUID=$(security find-identity -v -p codesigning "$KEYCHAIN" | head -1 | grep '"' | awk '{print $2}')
+IOS_UUID=$(security find-identity -v -p codesigning "$KEYCHAIN" | head -1 | 
+grep '"' | awk '{print $2}')
 
 security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN
